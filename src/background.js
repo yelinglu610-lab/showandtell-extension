68332f2d418fcb521cb4e7d30b894611f3eb01f7
yelinglu610-lab <yelinglu610@gmail.com>
2026-03-29 20:20:30 +0800

    update src/background.js

---
+++ src/background.js
@@ -0,0 +1,8 @@
+chrome.action.onClicked.addListener(async (tab) => {
+  try {
+    await chrome.scripting.insertCSS({ target: { tabId: tab.id }, files: ["src/content.css"] })
+    await chrome.scripting.executeScript({ target: { tabId: tab.id }, files: ["src/content.js"] })
+  } catch (e) {
+    console.error("SAT inject error:", e)
+  }
+})
